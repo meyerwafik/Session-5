fef0f7c5ff1b8d8c0ca11d529f8c8fe675e5aa3b
Meyer Wafik <49960083+meyerwafik@users.noreply.github.com>
2020-07-23 16:18:52 +0200

    Add files via upload

---
+++ Truffle_Task 1/contracts/Storage.sol
@@ -0,0 +1,20 @@
+contract Storage {
+
+    uint256 number;
+
+    /**
+     * @dev Store value in variable
+     * @param num value to store
+     */
+    function store(uint256 num) public {
+        number = num;
+    }
+
+    /**
+     * @dev Return value 
+     * @return value of 'number'
+     */
+    function retreive() public view returns (uint256){
+        return number;
+    }
+}
